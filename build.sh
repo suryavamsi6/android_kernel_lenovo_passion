ea00c77b5af92ff5cc5f24f61ad1fbc7f028af99
FireLord1 <firelord.xda@gmail.com>
2017-01-26 20:06:40 +0530

    script: misc update to simple build script
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 export ARCH=arm64
 export CROSS_COMPILE=~/android/kernel/toolchain/google-64-4.9/bin/aarch64-linux-android-
-make cyanogenmod_passion-row_defconfig
+make mrproper
+make lineage_p1a42_defconfig
+make -j10
